ea528cf0fd6da53418d68c7dff3aaaf2818de1b2
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-07-24 13:59:10 -0300

    Create estrutura-condicional.py

---
+++ estrutura-condicional.py
@@ -0,0 +1,15 @@
+# estruturas condicionais
+x = 1
+y = 2
+
+if x > y:
+    print("x eh maior que y")
+
+elif x > 3:
+    print("x eh maior que 3")
+
+elif x > 4:
+    print("x maior que 4")
+    
+else:
+    print("y eh maior que x")
